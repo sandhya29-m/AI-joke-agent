c6d7844f31b616c763eae86677645722c58395b1
Sandhya M <143873176+sandhya29-m@users.noreply.github.com>
2026-06-29 12:20:00 +0530

    Refactor Groq client initialization with Streamlit support
    Updated Groq client initialization to use Streamlit secrets if available.

--- tools.py
+++ tools.py
@@ -1,17 +1,21 @@
+import os
 from groq import Groq
 from dotenv import load_dotenv
-import os
+import streamlit as st
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+api_key = os.getenv("GROQ_API_KEY")
+
+# Use Streamlit Secrets if available
+if not api_key:
+    api_key = st.secrets["GROQ_API_KEY"]
+
+client = Groq(api_key=api_key)
 
 MODEL = "llama-3.1-8b-instant"
 
 def generate_joke(messages):
-
     response = client.chat.completions.create(
         model=MODEL,
         messages=messages,
